a15bd0cd0edeba6975e020d2fe8755d18bde3ce5
Alvaro <jlinaresmedalla@gmail.com>
2023-07-06 23:57:43 -0500

    comentarios finales

--- src/main/java/com/springcore/Cliente.java
+++ src/main/java/com/springcore/Cliente.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Cliente {
     public static void main(String[] args) {
+
         //De esta forma se puede cambiar el vehiculo sin tener que cambiar el codigo
         //y estariamos aplicando el principio de inversion de dependencias
         // ya no seria tighly coupled ( estrechamente acoplado ) sino loosely coupled ( debilmente acoplado )
@@ -17,11 +18,14 @@ public class Cliente {
 //        viajero2.comienzaViaje();
 //        viajero3.comienzaViaje();
 
+        // - ApplicationContext es una interfaz que implementa BeanFactory, lo que hace es generar el
+        //   contenedor de beans, es decir, Spring IoC Container.
+        // - Configura y maneja los beans segun las indicaciones de la clase de configuracion (AppConfiguracion)
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguracion.class);
+
+        // - Obtiene el bean de la clase Viajero del contenedor de beans (Spring IoC Container)
         Viajero viajero1 = applicationContext.getBean(Viajero.class);
         viajero1.comienzaViaje();
 
-        Auto auto = applicationContext.getBean(Auto.class);
-        auto.viajar();
     }
 }
